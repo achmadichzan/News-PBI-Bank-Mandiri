069e3b2264a2fa3f5fccd57cbf77939970f1b176
achmadichzan <122871669+achmadichzan@users.noreply.github.com>
2024-01-09 18:46:19 +0800

    Accidentally deletedd ApiService.kt :D

---
+++ app/src/main/java/com/achmadichzan/newspbibankmandiri/retrofit/ApiService.kt
@@ -0,0 +1,22 @@
+package com.achmadichzan.newspbibankmandiri.retrofit
+
+import com.achmadichzan.newspbibankmandiri.data.HeadlineResponse
+import com.achmadichzan.newspbibankmandiri.data.NewsResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("everything")
+    suspend fun getNews(
+        @Query("q") query: String,
+        @Query("from") fromDate: String,
+        @Query("sortBy") sortBy: String,
+    ): NewsResponse
+
+    @GET("top-headlines")
+    suspend fun getHeadlines(
+        @Query("country") country: String,
+        @Query("category") category: String
+    ): HeadlineResponse
+}
